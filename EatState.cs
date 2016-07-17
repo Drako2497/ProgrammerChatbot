04778ff94aa0847fcc1b54b16e78b7801dda3e89
Drako2497 <ngo20g@yahoo.com>
2016-07-17 16:35:00 -0700

    Created a state called EatState. Work in progress.

---
+++ EatState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammerChatbot
+{
+    public class EatState: Botstate
+    {
+        public EatState() : base("Eat")
+        {
+
+        }
+
+        public override void doAction(ProgrammerChatbot.Programmer bot)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
